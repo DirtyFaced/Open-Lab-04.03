ab32129a444bcb1a8e60541e3710e9f189582ed1
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-09 22:50:34 +0100

    ah

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 
 namespace Open_Lab_04._03
 {
@@ -6,7 +8,15 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            if (original.Length < 2)
+                return original;
+            char[] arrayout = new char[original.Length - 2];
+            for (int i = 1; i < original.Length - 1; i++)
+            {
+                arrayout[i - 1] = original[i];
+            }
+            string outt = new string(arrayout);
+            return outt;
         }
     }
 }
